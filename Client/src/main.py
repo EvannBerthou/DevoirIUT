a5fb1cf2ef97a57746bf0374c3782bb108b4d9a2
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-28 17:51:00 +0100

    Client

--- Client/src/main.py
+++ Client/src/main.py
@@ -1,9 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+import requests, json
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    return '<h1> Home </h1>'
-
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    devoirs_r = requests.get('http://localhost:5000/api/devoirs')
+    if devoirs_r.status_code == 200:
+        sortie = '<li>'
+        devoirs = json.loads(devoirs_r.content)
+        return render_template('devoirs.html', devoirs=devoirs)
+    else:
+        return '<h1> Erreur </h1>'
