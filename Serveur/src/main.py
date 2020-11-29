ee8563da01877df9678a5055dd06d8a2e15d3d45
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-29 14:46:15 +0100

    Extraction des classes dans la DB

--- Serveur/src/main.py
+++ Serveur/src/main.py
@@ -1,12 +1,6 @@
 from flask import Flask
 from api import api
-from bs4 import BeautifulSoup
-import requests
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
 
-html = requests.get('http://chronos.iut-velizy.uvsq.fr/EDT/gindex.html').content
-
-soup = BeautifulSoup(html, features='html.parser')
-classes = soup.find('select').text.strip().split('\n')[2:]
