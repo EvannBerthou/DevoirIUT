5fd3e3b6a611cd822b423ad98c9a3fc73c1213ad
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-29 15:33:14 +0100

    Afficher les devoirs par classe dans le tableau

--- Client/src/main.py
+++ Client/src/main.py
@@ -1,14 +1,26 @@
 import requests, json
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def home():
-    devoirs_r = requests.get('http://localhost:5000/api/devoirs')
+@app.route('/classe', methods=['GET'])
+def devoir_classe():
+    if not request.args:
+        return redirect('/')
+
+    classe = request.args['classe']
+    devoirs_r = requests.get('http://localhost:5000/api/devoirs', params={'classe': classe})
     if devoirs_r.status_code == 200:
-        sortie = '<li>'
         devoirs = json.loads(devoirs_r.content)
-        return render_template('devoirs.html', devoirs=devoirs)
+        return render_template('devoirs.html', devoirs=devoirs, classe=classe)
+    else:
+        return '<h1> Erreur </h1>'
+
+@app.route('/', methods=['GET'])
+def home():
+    classes_r = requests.get('http://localhost:5000/api/classe')
+    if classes_r.status_code == 200:
+        classes = [''.join(classe) for classe in json.loads(classes_r.content)]
+        return render_template('index.html', classes=classes)
     else:
         return '<h1> Erreur </h1>'
