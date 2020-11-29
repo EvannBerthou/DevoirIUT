43b80f4f0de3128ed6d3701fce062acb23c28291
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-29 14:46:15 +0100

    Extraction des classes dans la DB

--- Serveur/setup_db.py
+++ Serveur/setup_db.py
@@ -1,19 +1,33 @@
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
+
+html = requests.get('http://chronos.iut-velizy.uvsq.fr/EDT/gindex.html').content
+soup = BeautifulSoup(html, features='html.parser')
+classes = soup.find('select').text.strip().split('\n')[2:]
 
 db = sqlite3.connect('src/devoirs.db')
 c = db.cursor()
 
+c.execute('DROP TABLE IF EXISTS classes')
+c.execute('CREATE TABLE classes (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT);')
+
+for classe in classes:
+    c.execute('INSERT INTO classes (nom) VALUES (?);', [classe])
+
 c.execute("""
     CREATE TABLE devoirs (
         enonce TEXT,
+        classe INT,
         prof INT,
         jour date DEFAULT (date(datetime('now', '+1 day', 'localtime'))),
-        a_rendre INT DEFAULT 0
+        a_rendre INT DEFAULT 0,
+        FOREIGN KEY(classe) REFERENCES classes(id)
     );
 """)
 
 c.execute("""
-    INSERT INTO devoirs (enonce, prof) VALUES ('Devoir 1', 0), ('Devoir 2', 1);
+    INSERT INTO devoirs (enonce, classe, prof) VALUES ('Devoir 1', 3, 0), ('Devoir 2', 8, 1);
 """)
 
 c.close()
