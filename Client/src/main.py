9dcaf23642b022369d4cdd33e1e34fe17e378b48
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-29 16:30:34 +0100

    Choix de plusieurs classes

--- Client/src/main.py
+++ Client/src/main.py
@@ -16,11 +16,33 @@ def devoir_classe():
     else:
         return '<h1> Erreur </h1>'
 
-@app.route('/', methods=['GET'])
-def home():
+def liste_classes():
     classes_r = requests.get('http://localhost:5000/api/classe')
     if classes_r.status_code == 200:
         classes = [''.join(classe) for classe in json.loads(classes_r.content)]
+        return classes
+    return None
+
+@app.route('/', methods=['GET'])
+def home():
+    classes = liste_classes()
+    if classes is not None:
         return render_template('index.html', classes=classes)
     else:
         return '<h1> Erreur </h1>'
+
+@app.route('/nouveau', methods=['GET', 'POST'])
+def nouveau_devoir():
+    if request.method == 'GET':
+        classes = liste_classes()
+        if classes is not None:
+            return render_template('nouveau.html', classes=classes)
+        else:
+            return '<h1> Erreur </h1>'
+
+    elif request.method == 'POST':
+        classes = [key for key, val in request.form.items() if val == 'on']
+        enonce = request.form['enonce']
+        prof = request.form['prof']
+        requests.post('http://localhost:5000/api/devoirs', params={'enonce': enonce, 'prof': prof, 'classe': classes})
+        return redirect('/')
