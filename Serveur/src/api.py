6ff3053814692d0262d376afdd710a276ba770f1
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-11-29 15:50:58 +0100

    Formulaire pour envoyer un nouveau devoir

--- Serveur/src/api.py
+++ Serveur/src/api.py
@@ -7,16 +7,29 @@ def ajouter_devoir(args):
     print("Nouveau devoir")
     db = sqlite3.connect('src/devoirs.db')
     c = db.cursor()
-    enonce,prof = args["enonce"], args["prof"]
-    print(enonce, prof)
-    c.execute("INSERT INTO devoirs (enonce, prof) VALUES (?, ?);", [enonce, prof])
+    enonce,prof,classe = args["enonce"], args["prof"], args["classe"]
+
+    c.execute("""
+        INSERT INTO devoirs (enonce, prof, classe)
+        VALUES (?, ?, 
+            (SELECT id FROM classes WHERE nom = ?));
+    """,
+    [enonce, prof, classe])
+
     db.commit()
     return jsonify({}), 200
 
-def liste_devoirs():
+def liste_devoirs(id_classe):
     db = sqlite3.connect('src/devoirs.db')
     c = db.cursor()
-    rows = c.execute("SELECT * FROM devoirs")
+    rows = c.execute("""
+        SELECT enonce, prof, jour, 
+            REPLACE(REPLACE(a_rendre, 0, 'Non'), 1, 'Oui')
+        FROM devoirs, classes
+        WHERE devoirs.classe = classes.id
+        AND classes.nom = ?;
+    """,
+    [id_classe])
     devoirs = [row for row in rows]
     return jsonify(devoirs), 200
 
@@ -25,4 +38,14 @@ def devoirs():
     if request.method == 'POST':
         return ajouter_devoir(request.args)
     elif request.method == 'GET':
-        return liste_devoirs()
+        return liste_devoirs(request.args['classe'])
+
+@api.route('/classe', methods=['GET'])
+def classes():
+    db = sqlite3.connect('src/devoirs.db')
+    c = db.cursor()
+    rows = c.execute("""
+        SELECT nom FROM classes;
+    """)
+    classes = [row for row in rows]
+    return jsonify(classes), 200
