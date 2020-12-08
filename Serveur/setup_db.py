1802bb5e363e242154b23e8e618afd4b231213de
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-12-08 17:22:06 +0100

    Support de plusieurs pièces jointes

--- Serveur/setup_db.py
+++ Serveur/setup_db.py
@@ -12,9 +12,6 @@ c = db.cursor()
 c.execute('DROP TABLE IF EXISTS classes')
 c.execute('CREATE TABLE classes (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT NOT NULL);')
 
-c.execute('DROP TABLE IF EXISTS pj')
-c.execute('CREATE TABLE pj (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT NOT NULL, contenue BLOB NOT NULL);')
-
 for classe in classes:
     c.execute('INSERT INTO classes (nom) VALUES (?);', [classe])
 
@@ -28,11 +25,20 @@ c.execute("""
         prof INT,
         jour date DEFAULT (date(datetime('now', '+1 day', 'localtime'))),
         a_rendre INT DEFAULT 0,
-        pj INT,
-        FOREIGN KEY(classe) REFERENCES classes(id),
-        FOREIGN KEY(pj) REFERENCES pj(id)
+        FOREIGN KEY(classe) REFERENCES classes(id)
     );
 """)
+
+c.execute('DROP TABLE IF EXISTS pj')
+c.execute("""CREATE TABLE pj (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        devoir_id INT,
+        nom TEXT NOT NULL,
+        contenue BLOB NOT NULL,
+        FOREIGN KEY (devoir_id) REFERENCES devoirs(id)
+    );
+""")
+
 
 c.execute('DROP TABLE IF EXISTS enseignant')
 c.execute("""
