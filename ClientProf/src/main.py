653ea45c729e7ac5a699a50d40ced99e4d44caff
Sutalite <sutalite@DESKTOP-C0OBNN4.localdomain>
2020-12-12 18:32:56 +0100

    Simplification des clients

--- ClientProf/src/main.py
+++ ClientProf/src/main.py
@@ -6,8 +6,7 @@ app = Flask(__name__, template_folder='templates')
 def liste_classes():
     classes_r = requests.get('http://localhost:5000/api/classe')
     if classes_r.status_code == 200:
-        classes = [''.join(classe) for classe in json.loads(classes_r.content)]
-        return classes
+        return [''.join(classe) for classe in json.loads(classes_r.content)]
     return None
 
 @app.route('/')
@@ -26,24 +25,18 @@ def nouveau_devoir():
     elif request.method == 'POST':
         classes = [key for key, val in request.form.items() if val == 'on']
         enonce = request.form['enonce']
-        matiere=request.form['matiere']
+        matiere = request.form['matiere']
         prof = request.form['prof']
         date = request.form['date']
-        pj = []
-        for file in request.files.getlist('file'):
-            if file.filename != '':
-                pj.append(file)
-                file.save(os.path.join('.', file.filename))
 
         files = {}
-        for file in pj:
-            files[file.filename] = open(file.filename, 'rb').read()
+        for file in request.files.getlist('file'):
+            if file.filename != '':
+                files[file.filename] = file.stream.read()
 
         requests.post('http://localhost:5000/api/devoirs', 
-            params={'enonce': enonce,'matiere':matiere, 'prof': prof, 'classe': classes, 'date': date},
+            params={'enonce': enonce,'matiere': matiere, 'prof': prof, 'classe': classes, 'date': date},
             files=files
         )
         
-        [os.remove(file.filename) for file in pj]
-
         return redirect('/')
