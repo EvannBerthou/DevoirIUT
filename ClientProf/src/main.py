63e360a2183bce75d8c7cb9ad36763746e0f2974
Sutalite <nedje28@gmail.com>
2020-12-25 22:02:54 +0100

    Ajout de la génération de JWT côté serveur

--- ClientProf/src/main.py
+++ ClientProf/src/main.py
@@ -1,26 +1,19 @@
-import requests, json, os, re, io, flask_login
+import requests, json, os, re, io
 from flask import Flask, request, render_template, redirect, send_file
 from admin import admin
 
 app = Flask(__name__, template_folder='templates')
 app.register_blueprint(admin, url_prefix='/admin')
 app.secret_key = 'secret key'
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-class User(flask_login.UserMixin):
-# TODO: Ajouter des infos tels que le nom au lieu de mettre le nom directement dans l'id
-    pass
-
 def liste_classes():
     classes_r = requests.get('http://localhost:5000/api/classe')
     if classes_r.status_code == 200:
         return [''.join(classe) for classe in json.loads(classes_r.content)]
     return None
 
-def liste_matires(enseignant):
+def liste_matires():
     matieres_r = requests.get('http://localhost:5000/api/matieres',
-            params={'enseignant':enseignant})
+            params={'enseignant':'c'})
     if matieres_r.status_code == 200:
         return [''.join(matiere) for matiere in json.loads(matieres_r.content)]
     return None
@@ -30,14 +23,12 @@ def home():
     return redirect('/login')
 
 @app.route('/nouveau', methods=['GET', 'POST'])
-@flask_login.login_required
 def nouveau_devoir():
     if request.method == 'GET':
         classes = liste_classes()
-        matieres = liste_matires(flask_login.current_user.id)
+        matieres = liste_matires()
         if classes:
-            return render_template('nouveau.html', user = flask_login.current_user,
-                    import flask_login
+            return render_template('nouveau.html', user = 'c')
                 classes=classes, matieres=matieres)
         else:
             return '<h1> Erreur </h1>'
@@ -46,16 +37,12 @@ def nouveau_devoir():
         classes = [key for key, val in request.form.items() if val == 'on']
         enonce = request.form['enonce']
         matiere = request.form['matiere']
-        prof = flask_login.current_user.id # Le nom du prof est l'id, devra être changé pour utiliser current_user.nom
+        prof = 'c' #flask_login.current_user.id # Le nom du prof est l'id, devra être changé pour utiliser current_user.nom
         date = request.form['date']
 
 
 
         #files = {file.filename: file.stream.read() for file in request.files.getlist('file') if file.filename != ''}
-        files = {}
-        for file in request.files.getlist('file'):
-            if file.filename != '':
-                files[file.filename] = file.stream.read()
 
         requests.post('http://localhost:5000/api/devoirs',
             params={'enonce': enonce,'matiere': matiere, 'prof': prof, 'classe': classes, 'date': date},
@@ -65,20 +52,18 @@ def nouveau_devoir():
         return redirect('/devoirs')
 
 @app.route('/devoirs',methods=['GET', 'POST'])
-@flask_login.login_required
 def affichage_devoirs():
     if request.method =='GET':
-        devoirs_r = requests.get('http://localhost:5000/api/devoirs', params={'user': flask_login.current_user.id})
+        devoirs_r = requests.get('http://localhost:5000/api/devoirs', params={'user': flask_hhogin.current_user.id})
         if devoirs_r.status_code == 200:
             devoirs = json.loads(devoirs_r.content)
-            return render_template('devoirs.html', devoirs=devoirs,user = flask_login.current_user)
+            return render_template('devoirs.html', devoirs=devoirs, user = 'c')
         else:
             return '<h1> Erreur </h1>'
     elif request.method=='POST':
         if 'delete_button' in request.form:
             requests.post('http://localhost:5000/api/sup', params={'devoir_id':request.form['delete_button']})
         else:
-            # TODO: Pouvoir changer la matière d'un devoir et les classes concernées
             params = {'devoir_id': request.form['devoir_id'],
                       'enonce': request.form['enonce'],
                       'date': request.form['date']}
@@ -97,34 +82,16 @@ def connexion():
         connect_data = requests.get('http://localhost:5000/api/login', params={'email': email, 'pwd': pwd})
         #recuperation des données de la personne conectée nom , prenom
         if connect_data.status_code == 200:
-            user = User()
-            user.id = email
-            flask_login.login_user(user)
+            #user = User()
+            #user.id = email
+            #flask_login.login_user(user)
             return redirect('/devoirs')
         else:
             return render_template('login.html', Erreur=True)
 
-@login_manager.user_loader
-def user_loader(email):
-    if not email:
-        return
-
-    user_data = requests.get('http://localhost:5000/api/user', params={'email': email})
-    if user_data.status_code != 200:
-        return None
-
-    user = User()
-    user.id = email
-    return user
-
 @app.route('/logout')
 def logout():
-    flask_login.logout_user()
     return redirect('/')
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return 'Accès refusé'
 
 @app.route('/pj')
 def pj():
--- Serveur/src/main.py
+++ Serveur/src/main.py
@@ -1,19 +1,41 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, jsonify
 from api import api
-import flask_login
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+from werkzeug.security import generate_password_hash, check_password_hash, safe_str_cmp
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
+app.secret_key = 'secret key'
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
 
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
 
-@login_manager.user_loader
-def user_loader():
-    user = User()
-    user.id = 0
-    return user
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
 
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return 'Accès refusé'
+    db = sqlite3.connect('src/devoirs.db')
+    c = db.cursor()
+    pass_found = c.execute('SELECT id, mail, pwd FROM enseignant WHERE mail = ?;', [username]).fetchone()
+    if pass_found and check_password_hash(pass_found[2], password):
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    return jsonify({"error": "Bad username or password"}), 401
+
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
