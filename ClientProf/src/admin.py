addaea230a7d59e0bbe78c38f0021a82ca52860a
Sutalite <nedje28@gmail.com>
2021-01-09 15:49:19 +0100

    Ajout de type hinting

--- ClientProf/src/admin.py
+++ ClientProf/src/admin.py
@@ -1,53 +1,59 @@
 import requests, json
+from typing import *
 from flask import Blueprint, request, jsonify, render_template, redirect
+from werkzeug.wrappers import Response
 
 admin = Blueprint('admin', __name__)
 
+Str_response = Tuple[str, int]
+Str_or_Response = Tuple[Union[str, Response], int]
+Response_code = Tuple[Response, int]
+
 # Vérifie si l'utilisateur connecté à le rôle d'admin
-def valid_access():
+def valid_access() -> Optional[Any]:
     role_r = requests.get('http://localhost:5000/api/role', cookies=request.cookies)
     if role_r.status_code == 200:
         return json.loads(role_r.content)
     return None
 
 # Liste des classes
-def liste_classes():
+def liste_classes() -> Optional[List[Tuple[int, str]]]:
     classes_r = requests.get('http://localhost:5000/api/classe')
     if classes_r.status_code == 200:
         return [classe for classe in json.loads(classes_r.content)]
     return None
 
 # Liste des enseignants
-def liste_enseignants():
+def liste_enseignants() -> Optional[List[List[str]]]:
     enseignants_r = requests.get('http://localhost:5000/api/enseignant')
     if enseignants_r.status_code == 200:
         return [[str(v) for v in enseignant] for enseignant in json.loads(enseignants_r.content)]
     return None
 
 # Liste de toutes les matières
-def liste_matieres():
+def liste_matieres() -> Optional[List[Tuple[int, str]]]:
     matieres_r = requests.get('http://localhost:5000/api/matieres')
     if matieres_r.status_code == 200:
         return [matieres for matieres in json.loads(matieres_r.content)]
     return None
 
 # Liste des classes d'un enseignant
-def classe_enseignants():
+def classe_enseignants() -> Optional[Any]:
     ce_r = requests.get('http://localhost:5000/api/classe_enseignant', cookies=request.cookies)
     if ce_r.status_code == 200:
         return json.loads(ce_r.content)
     return None
 
 # Liste des matières d'un enseignant
-def matiere_enseignants():
+def matiere_enseignants() -> Optional[Any]:
     ce_r = requests.get('http://localhost:5000/api/matiere_enseignant', cookies=request.cookies)
     if ce_r.status_code == 200:
         return json.loads(ce_r.content)
     return None
 
-def merge_with_prof(d, profs):
+def merge_with_prof(d: List[Tuple[int, str]], profs: List[str]) -> List:
     id_d = {v:i for i,v in d}
-    d_avec_profs = {k:[] for k in [c[1] for c in d]}
+    d_avec_profs: Dict = {k:[] for k in [c[1] for c in d]}
 
     for x in profs:
         d_avec_profs[x[0]].append(x[1:])
@@ -56,7 +62,7 @@ def merge_with_prof(d, profs):
     d_avec_profs_et_id = [[id_d[d[0]]] + list(d) for d in d_avec_profs.items()]
     return d_avec_profs_et_id
 
-def send_form_to(url):
+def send_form_to(url: str) -> None:
     # Modification
     if 'id' in request.form:
         requests.patch(url, cookies=request.cookies,
@@ -74,9 +80,9 @@ def send_form_to(url):
         requests.delete(url, cookies=request.cookies, params={'id': request.form['suppr']})
 
 @admin.route('/')
-def dashboard():
+def dashboard() -> Str_response:
     if (resp := valid_access()):
-        return render_template('admin.html', user = resp['user'])
+        return render_template('admin.html', user = resp['user']), 200
     return render_template('error.html', msg="Accès refusé"), 404
 
 """
@@ -85,21 +91,22 @@ Classes
 
 # Affiche la liste de toutes les classes (+ les profs dans chaque classe)
 @admin.route('/classes', methods=['GET'])
-def get_classes():
+def get_classes() -> Str_response:
     if (resp := valid_access()):
         classes = liste_classes()
         enseignants = liste_enseignants()
         ce = classe_enseignants()
-        merged = merge_with_prof(classes, ce)
-        return render_template('classes.html', user = resp['user'], enseignants=enseignants, classes=merged)
+        if classes and ce:
+            merged = merge_with_prof(classes, ce)
+            return render_template('classes.html', user = resp['user'], enseignants=enseignants, classes=merged), 200
     return render_template('error.html', msg="Accès refusé"), 404
 
 # Modification d'une classe
 @admin.route('/classes', methods=['POST'])
-def post_classes():
+def post_classes() -> Str_or_Response:
     if (resp := valid_access()):
         send_form_to('http://localhost:5000/api/gestion_classe')
-        return redirect('/admin/classes')
+        return redirect('/admin/classes'), 200
     return render_template('error.html', msg="Accès refusé"), 404
 
 """
@@ -108,21 +115,22 @@ Matières
 
 # Affiche la liste de toutes les matières (+ les profs dans chaque matières)
 @admin.route('/matieres', methods=['GET'])
-def get_matieres():
+def get_matieres() -> Str_response:
     if (resp := valid_access()):
         matieres = liste_matieres()
         enseignants = liste_enseignants()
         ce = matiere_enseignants()
-        merged = merge_with_prof(matieres, ce)
-        return render_template('matieres.html', user = resp['user'], enseignants=enseignants, matieres=merged)
+        if matieres and ce:
+            merged = merge_with_prof(matieres, ce)
+            return render_template('matieres.html', user = resp['user'], enseignants=enseignants, matieres=merged), 200
     return render_template('error.html', msg="Accès refusé"), 404
 
 
 @admin.route('/matieres', methods=['POST'])
-def post_matieres():
+def post_matieres() -> Str_or_Response:
     if (resp := valid_access()):
         send_form_to('http://localhost:5000/api/gestion_matieres')
-        return redirect('/admin/matieres')
+        return redirect('/admin/matieres'), 200
     return render_template('error.html', msg="Accès refusé"), 404
 
 """
@@ -131,15 +139,15 @@ Enseignants
 
 # Affiche la liste de tous les profs (+ leurs infos : nom, prenom, mail)
 @admin.route('/enseignants', methods=['GET'])
-def get_enseignants():
+def get_enseignants() -> Str_response:
     if (resp := valid_access()):
         enseignants = liste_enseignants()
-        return render_template('enseignant.html', user = resp['user'], enseignants=enseignants)
+        return render_template('enseignant.html', user = resp['user'], enseignants=enseignants), 200
     return render_template('error.html', msg="Accès refusé"), 404
 
 @admin.route('/enseignants', methods=['POST'])
-def post_enseignants():
+def post_enseignants() -> Str_or_Response:
     if (resp := valid_access()):
         send_form_to('http://localhost:5000/api/gestion_enseignant')
-        return redirect('/admin/enseignants')
+        return redirect('/admin/enseignants'), 200
     return render_template('error.html', msg="Accès refusé"), 404
--- ClientProf/src/main.py
+++ ClientProf/src/main.py
@@ -1,56 +1,63 @@
 import requests, json, os, re, io
+from typing import *
 from flask import Flask, request, render_template, redirect, send_file, make_response
+from werkzeug.wrappers import Response
 from admin import admin
 
 app = Flask(__name__, template_folder='templates')
 app.register_blueprint(admin, url_prefix='/admin')
 app.secret_key = 'secret key'
 
+Str_response = Tuple[str, int]
+Str_or_Response = Tuple[Union[str, Response], int]
+Response_code = Tuple[Response, int]
+List_or_error = Tuple[Union[List[str], str], bool]
+
 # Liste de toutes les classes
-def liste_classes():
+def liste_classes() -> Optional[List[str]]:
     classes_r = requests.get('http://localhost:5000/api/classe')
     if classes_r.status_code == 200:
         return [''.join(classe) for classe in json.loads(classes_r.content)]
     return None
 
-# Liste des classes d'un prof ou message d'erreur + 0 si pas d'erreur sinon 1
-def liste_classes_prof():
+# Liste des classes d'un prof ou message d'erreur + erreur
+def liste_classes_prof() -> List_or_error:
     classes_r = requests.get('http://localhost:5000/api/classe', cookies=request.cookies)
     content = json.loads(classes_r.content)
     if classes_r.status_code == 200:
-        return [''.join(classe) for classe in content], 0
-    return content['msg'], 1
+        return [''.join(classe) for classe in content], False
+    return content['msg'], True
 
 # Liste de toutes les matières
-def liste_matieres():
+def liste_matieres() -> List_or_error:
     matieres_r = requests.get('http://localhost:5000/api/matieres', cookies=request.cookies)
     content = json.loads(matieres_r.content)
     if matieres_r.status_code == 200:
-        return [''.join(classe) for classe in content], 0
-    return content['msg'], 1
+        return [''.join(classe) for classe in content], False
+    return content['msg'], True
 
 @app.route('/')
-def home():
+def home() -> Response:
     return redirect('/login')
 
 @app.route('/nouveau', methods=['GET'])
-def get_nouveau():
+def get_nouveau() -> Str_response:
     result_classes, err_classe = liste_classes_prof()
     result_mat, err_mat = liste_matieres()
     if not err_classe and not err_mat:
-        return render_template('nouveau.html', user = 'c', classes=result_classes, matieres=result_mat)
+        return render_template('nouveau.html', user = 'c', classes=result_classes, matieres=result_mat), 200
     # Forme un message d'erreur
     s = '<h1>'
     if err_classe:
-        s += result_classes
+        s += str(result_classes)
         s += '<br>'
     if err_mat:
-        s += result_mat
+        s += str(result_mat)
     s += '</h1>'
-    return s
+    return s, 404
 
 @app.route('/nouveau', methods=['POST'])
-def post_nouveau():
+def post_nouveau() -> Response:
     classes = ''.join([key+',' for key, val in request.form.items() if val == 'on'])
     enonce = request.form['enonce']
     matiere = request.form['matiere']
@@ -66,16 +73,16 @@ def post_nouveau():
     return redirect('/devoirs')
 
 @app.route('/devoirs',methods=['GET'])
-def affichage_devoirs():
+def affichage_devoirs() -> Str_response:
     resp_r = requests.get('http://localhost:5000/api/devoirs', cookies=request.cookies)
     if resp_r.status_code == 200:
         resp = json.loads(resp_r.content)
         classes=liste_classes()
-        return render_template('devoirs.html', devoirs=resp['devoirs'], user = resp['user'], classes=classes)
-    return '<h1> Erreur </h1>'
+        return render_template('devoirs.html', devoirs=resp['devoirs'], user = resp['user'], classes=classes), 200
+    return '<h1> Erreur </h1>', 404
 
 @app.route('/devoirs',methods=['POST'])
-def post_devirs():
+def post_devoirs() -> Response:
     if 'delete_button' in request.form:
         requests.post('http://localhost:5000/api/sup', params={'devoir_id':request.form['delete_button']})
     else:
@@ -89,33 +96,32 @@ def post_devirs():
     return redirect('/devoirs')
 
 @app.route('/login',methods=['GET'])
-def get_login():
-    return render_template('login.html', Erreur=False)
+def get_login() -> Str_response:
+    return render_template('login.html', Erreur=False), 200
 
 @app.route('/login',methods=['POST'])
-def post_login():
+def post_login() -> Str_or_Response:
     email, pwd = request.form['username'], request.form['pwd']
     connect_data = requests.post('http://localhost:5000/api/token/auth', json={'username': email, 'password': pwd})
     #recuperation des données de la personne conectée nom , prenom
     if connect_data.status_code == 200:
         response = make_response(redirect('/devoirs'))
         response.set_cookie('access_token_cookie', connect_data.cookies.get('access_token_cookie'))
-        return response
-    return render_template('login.html', Erreur=True)
+        return response, 200
+    return render_template('login.html', Erreur=True), 404
 
 @app.route('/logout')
-def logout():
+def logout() -> Response:
     requests.post('http://localhost:5000/api/token/remove')
     resp = make_response(redirect('/'))
     resp.delete_cookie('access_token_cookie')
     return resp
 
 @app.route('/pj')
-def download_pj():
+def download_pj() -> Response:
     # Fait la requête au serveur pour récupérer le pj par id
     pj_r = requests.get('http://localhost:5000/api/pj', params={'id': request.args['pj']})
     # Récupère le nom dans le headers
     filename = re.findall("filename=(.+)", pj_r.headers['content-disposition'])[0].replace('"', '')
     # as_attachment signifie que le navigateur va télécharger le fichier au lieu d'essayer de l'afficher
-    return send_file(io.BytesIO(pj_r.content), as_attachment=True, attachment_filename=filename), 200
-
+    return send_file(io.BytesIO(pj_r.content), as_attachment=True, attachment_filename=filename)
